8ec8a2d30c9c122df4cf054e49740f43b9200c5a
Howay <heweihzl@163.com>
2019-10-20 16:09:46 +0800

    分页结果封装实体已实现

--- pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
+++ pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.pinyougou.pojo.TbBrand;
 import com.pinyougou.sellergoods.service.BrandService;
+import entity.PageResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,11 @@ public class BrandController {
 	@RequestMapping("/findAll")
 	public List<TbBrand> findAll(){
 		return brandService.findAll();		
+	}
+
+	@RequestMapping("/findPage")
+	public PageResult findPage(int pageNum, int pageSize){
+		return brandService.findPage(pageNum,pageSize);
 	}
 	
 }
