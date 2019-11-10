a8aac483f7451c28be776fe8e1e0d3fd1d8f90ba
Howay <heweihzl@163.com>
2019-11-10 12:10:01 +0800

    显示分类完成

--- pinyougou-shop-web/src/main/webapp/js/controller/goodsController.js
+++ pinyougou-shop-web/src/main/webapp/js/controller/goodsController.js
@@ -218,4 +218,17 @@ app.controller('goodsController' ,function($scope,$controller,goodsService,uploa
 
 	$scope.status = ['未审核','已审核','审核未通过','驳回']  // 商品状态
 
+	$scope.itemCatList =[]; //商品列表分类
+
+	// 查询商品分类列表
+	$scope.findItemCatList = function () {
+		itemCatService.findAll().success(
+			function (response) {
+				for (var i=0; i<response.length;i++){
+					$scope.itemCatList[response[i].id] = response[i].name;
+				}
+			}
+		)
+	}
+
 });	
