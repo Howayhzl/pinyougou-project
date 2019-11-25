abe2ff45290ca8e1e77cc95be78e05853972262f
Howay <heweihzl@163.com>
2019-11-25 23:18:54 +0800

    增加查询条件完成

--- pinyougou-search-web/src/main/webapp/js/controller/searchController.js
+++ pinyougou-search-web/src/main/webapp/js/controller/searchController.js
@@ -1,7 +1,7 @@
 app.controller('searchController',function ($scope, searchService) {
 
     // 定义搜索对象结构 category：商品分类
-    $scope.searchMap = {'keywords':'','category':'','brand':'','spec':{}};
+    $scope.searchMap={'keywords':'','category':'','brand':'','spec':{}};//搜索对象
 
     //搜索
     $scope.search = function () {
@@ -10,6 +10,15 @@ app.controller('searchController',function ($scope, searchService) {
                 $scope.resultMap = response;
             }
         );
+    }
+
+    //添加搜索项
+    $scope.addSearchItem=function(key,value){
+        if(key=='category' || key=='brand'){//如果点击的是分类或者是品牌
+            $scope.searchMap[key]=value;
+        }else{
+            $scope.searchMap.spec[key]=value;
+        }
     }
 
 })
