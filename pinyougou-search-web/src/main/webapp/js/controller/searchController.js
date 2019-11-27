25a5f4bff30278e7c438a1175ac8c9415d3758c4
Howay <heweihzl@163.com>
2019-11-27 22:15:16 +0800

    构建分页栏1完成

--- pinyougou-search-web/src/main/webapp/js/controller/searchController.js
+++ pinyougou-search-web/src/main/webapp/js/controller/searchController.js
@@ -1,15 +1,25 @@
 app.controller('searchController',function ($scope, searchService) {
 
     // 定义搜索对象结构 category：商品分类
-    $scope.searchMap={'keywords':'','category':'','brand':'','spec':{},'price':''};//搜索对象
+    $scope.searchMap={'keywords':'','category':'','brand':'','spec':{},'price':'','pageNo':1,'pageSize':20};//搜索对象
 
     //搜索
     $scope.search = function () {
         searchService.search($scope.searchMap).success(
             function (response) {
                 $scope.resultMap = response;
+
+                buildPageLable()// 构建分页栏
             }
         );
+    }
+
+    buildPageLable = function(){
+        // 构建分页栏
+        $scope.pageLable=[];
+        for (var i=1;i<$scope.resultMap.totalPages;i++){
+            $scope.pageLable.push(i);
+        }
     }
 
     //添加搜索项
