d27e34faa4dec286ed1869ae7218e23cc7154eee
Howay <heweihzl@163.com>
2019-11-30 19:30:33 +0800

    搜索页与首页对接

--- pinyougou-search-web/src/main/webapp/js/controller/searchController.js
+++ pinyougou-search-web/src/main/webapp/js/controller/searchController.js
@@ -1,4 +1,4 @@
-app.controller('searchController',function ($scope, searchService) {
+app.controller('searchController',function ($scope,$location, searchService) {
 
     // 定义搜索对象结构 category：商品分类
     $scope.searchMap={'keywords':'','category':'','brand':'','spec':{},'price':'','pageNo':1,'pageSize':20,'sort':'','sortField':''};//搜索对象
@@ -113,5 +113,12 @@ app.controller('searchController',function ($scope, searchService) {
         }
         return  false;
      }
+
+    //加载查询字符串
+    $scope.loadkeywords=function(){
+        $scope.searchMap.keywords=  $location.search()['keywords'];
+        $scope.search();
+    }
+
 
 })
