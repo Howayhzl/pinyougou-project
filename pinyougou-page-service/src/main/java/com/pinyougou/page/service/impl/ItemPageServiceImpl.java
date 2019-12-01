ab515a2f20f75fd4c5e57604e4412f17ab1707ea
Howay <heweihzl@163.com>
2019-12-01 12:47:55 +0800

    生成商品类型面包屑(商品分类完成)

--- pinyougou-page-service/src/main/java/com/pinyougou/page/service/impl/ItemPageServiceImpl.java
+++ pinyougou-page-service/src/main/java/com/pinyougou/page/service/impl/ItemPageServiceImpl.java
@@ -3,6 +3,7 @@ package com.pinyougou.page.service.impl;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.pinyougou.mapper.TbGoodsDescMapper;
 import com.pinyougou.mapper.TbGoodsMapper;
+import com.pinyougou.mapper.TbItemCatMapper;
 import com.pinyougou.page.service.ItemPageService;
 import com.pinyougou.pojo.TbGoods;
 import com.pinyougou.pojo.TbGoodsDesc;
@@ -34,6 +35,9 @@ public class ItemPageServiceImpl implements ItemPageService {
     @Autowired
     private TbGoodsDescMapper goodsDescMapper;
 
+    @Autowired
+    private TbItemCatMapper itemCatMapper;
+
     @Override
     public boolean generateHtml(Long goodsId) {
         Configuration configuration = freeMarkerConfig.getConfiguration();
@@ -41,12 +45,20 @@ public class ItemPageServiceImpl implements ItemPageService {
             Template template = configuration.getTemplate("item.ftl");
             // 创建数据模型
             Map dataModel = new HashMap();
-            // 商品主表数据
+            // 1.商品主表数据
             TbGoods goods = goodsMapper.selectByPrimaryKey(goodsId);
             dataModel.put("goods",goods);
-            //商品扩展表数据
+            //2.商品扩展表数据
             TbGoodsDesc goodsDesc = goodsDescMapper.selectByPrimaryKey(goodsId);
             dataModel.put("goodsDesc",goodsDesc);
+            // 3.商品分类（生成面包屑）
+            String Category1 = itemCatMapper.selectByPrimaryKey(goods.getCategory1Id()).getName();
+            String Category2 = itemCatMapper.selectByPrimaryKey(goods.getCategory2Id()).getName();
+            String Category3 = itemCatMapper.selectByPrimaryKey(goods.getCategory3Id()).getName();
+
+            dataModel.put("Category1",Category1);
+            dataModel.put("Category2",Category2);
+            dataModel.put("Category3",Category3);
 
             OutputStreamWriter out = new OutputStreamWriter(new FileOutputStream(pagedir+goodsId+".html"),"utf-8");
 
