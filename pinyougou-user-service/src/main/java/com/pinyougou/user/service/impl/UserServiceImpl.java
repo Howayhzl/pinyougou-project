c4ac1ff5bdec05c3253d06b25ae79a080ba0ab46
Howay <heweihzl@163.com>
2019-12-07 21:54:48 +0800

    用户基本功能注册后端完成

--- pinyougou-user-service/src/main/java/com/pinyougou/user/service/impl/UserServiceImpl.java
+++ pinyougou-user-service/src/main/java/com/pinyougou/user/service/impl/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package com.pinyougou.user.service.impl;
+import java.util.Date;
 import java.util.List;
 
 import com.pinyougou.mapper.TbUserMapper;
 import com.pinyougou.pojo.TbUser;
 import com.pinyougou.user.service.UserService;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.github.pagehelper.Page;
@@ -48,6 +50,10 @@ public class UserServiceImpl implements UserService {
 	 */
 	@Override
 	public void add(TbUser user) {
+		user.setCreated(new Date()); //用户注册时间
+		user.setUpdated(new Date()); // 修改时间
+		user.setSourceType("1"); // 注册来源PC
+		user.setPassword(DigestUtils.md5Hex(user.getPassword())); //密码加密
 		userMapper.insert(user);		
 	}
 
