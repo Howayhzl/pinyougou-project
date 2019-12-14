49f4543393af254069fc95e8c0933820bf6bc689
Howay <heweihzl@163.com>
2019-12-14 21:37:31 +0800

    跨域处理跳转购物车页面测试完成

--- pinyougou-cart-web/src/main/java/com/pinyougou/cart/controller/CartController.java
+++ pinyougou-cart-web/src/main/java/com/pinyougou/cart/controller/CartController.java
@@ -69,6 +69,10 @@ public class CartController {
 
     @RequestMapping("/addGoodsToCartList")
     public Result addGoodsToCartList(Long itemId, Integer num){
+
+        response.setHeader("Access-Control-Allow-Origin","http://localhost:9105"); // 可以访问的域（不需要操作cookie）
+        response.setHeader("Access-Control-Allow-Credentials","true"); // 如果操作cookie，必须加上这句话
+
         // 当前登陆人账号，判断当前是否有人登陆
         String name = SecurityContextHolder.getContext().getAuthentication().getName();
         System.out.println("当前登陆人："+name);
--- pinyougou-page-web/src/main/webapp/js/controller/itemController.js
+++ pinyougou-page-web/src/main/webapp/js/controller/itemController.js
@@ -62,7 +62,7 @@ app.controller('itemController',function ($scope,$http) {
 	// 添加商品到购物车
 	$scope.addToCart=function(){
 		// alert('SKUID:'+$scope.sku.id)
-		$http.get('http://localhost:9107/cart/addGoodsToCartList.do?itemId='+$scope.sku.id+'&num='+$scope.num).success(
+		$http.get('http://localhost:9107/cart/addGoodsToCartList.do?itemId='+$scope.sku.id+'&num='+$scope.num,{'withCredentials':true}).success(
 			function (response) {
 				if (response.success){
 					location.href='http://localhost:9107/cart.html';
