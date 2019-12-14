49c4360ab337ac1b7460b6335c12cdb20207629c
Howay <heweihzl@163.com>
2019-12-14 12:03:07 +0800

    购物车前端控制层数量增显示总数量和总费用成功

--- pinyougou-cart-web/src/main/webapp/js/controller/cartController.js
+++ pinyougou-cart-web/src/main/webapp/js/controller/cartController.js
@@ -6,6 +6,7 @@ app.controller('cartController',function ($scope, cartService) {
         cartService.findCartList().success(
             function (resposne) {
                 $scope.cartList=resposne;
+                sun()
             }
         )
     }
@@ -21,5 +22,21 @@ app.controller('cartController',function ($scope, cartService) {
                 }
             }
         )
+    }
+
+    // 求合计
+    sum = function () {
+        $scope.totalNum = 0; //总数量
+        $scope.totalMoney = 0; // 总金额
+
+        for (var i=0; i<$scope.cartList.length;i++){
+            var cart = $scope.cartList[i]; //购物车对象
+            for (var j=0; j<cart.orderItemList.length; j++){
+                var orderItem = cart.orderItemList[i]; // 购物车明细
+                $scope.totalNum += orderItem.num; // 累加数量
+                $scope.totalMoney += orderItem.totalFee; // 累加金额
+
+            }
+        }
     }
 })
