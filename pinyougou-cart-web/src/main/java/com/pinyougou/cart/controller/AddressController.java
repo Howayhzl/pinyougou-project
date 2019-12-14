61491f5651bb8a1d191ef471dc1bce1369c215ed
Howay <heweihzl@163.com>
2019-12-14 21:56:45 +0800

    获取收获人地址列表后端完成

---
+++ pinyougou-cart-web/src/main/java/com/pinyougou/cart/controller/AddressController.java
@@ -0,0 +1,123 @@
+package com.pinyougou.cart.controller;
+import java.util.List;
+
+import com.pinyougou.pojo.TbAddress;
+import com.pinyougou.user.service.AddressService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.alibaba.dubbo.config.annotation.Reference;
+
+
+import entity.PageResult;
+import entity.Result;
+/**
+ * controller
+ * @author Administrator
+ *
+ */
+@RestController
+@RequestMapping("/address")
+public class AddressController {
+
+	@Reference
+	private AddressService addressService;
+	
+	/**
+	 * 返回全部列表
+	 * @return
+	 */
+	@RequestMapping("/findAll")
+	public List<TbAddress> findAll(){
+		return addressService.findAll();
+	}
+	
+	
+	/**
+	 * 返回全部列表
+	 * @return
+	 */
+	@RequestMapping("/findPage")
+	public PageResult  findPage(int page,int rows){			
+		return addressService.findPage(page, rows);
+	}
+	
+	/**
+	 * 增加
+	 * @param address
+	 * @return
+	 */
+	@RequestMapping("/add")
+	public Result add(@RequestBody TbAddress address){
+		try {
+			addressService.add(address);
+			return new Result("增加成功",true);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new Result("增加失败",false);
+		}
+	}
+	
+	/**
+	 * 修改
+	 * @param address
+	 * @return
+	 */
+	@RequestMapping("/update")
+	public Result update(@RequestBody TbAddress address){
+		try {
+			addressService.update(address);
+			return new Result("修改成功",true);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new Result( "修改失败",false);
+		}
+	}	
+	
+	/**
+	 * 获取实体
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping("/findOne")
+	public TbAddress findOne(Long id){
+		return addressService.findOne(id);		
+	}
+	
+	/**
+	 * 批量删除
+	 * @param ids
+	 * @return
+	 */
+	@RequestMapping("/delete")
+	public Result delete(Long [] ids){
+		try {
+			addressService.delete(ids);
+			return new Result("删除成功",true);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new Result("删除失败",false);
+		}
+	}
+	
+		/**
+	 * 查询+分页
+	 * @param address
+	 * @param page
+	 * @param rows
+	 * @return
+	 */
+	@RequestMapping("/search")
+	public PageResult search(@RequestBody TbAddress address, int page, int rows  ){
+		return addressService.findPage(address, page, rows);		
+	}
+
+
+	@RequestMapping("/findListByLoginUser")
+	public List<TbAddress> findListByLoginUser(){
+		// 获取登陆用户名
+		String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+		return addressService.findAddressByUserId(userName);
+	}
+}
