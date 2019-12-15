c7a3ffd4cfbd480b786b5a7b31a0ff6011474cf8
Howay <heweihzl@163.com>
2019-12-15 12:44:35 +0800

    保存订单服务层已完成

--- pinyougou-order-service/src/main/java/com/pinyougou/order/service/impl/OrderServiceImpl.java
+++ pinyougou-order-service/src/main/java/com/pinyougou/order/service/impl/OrderServiceImpl.java
@@ -1,9 +1,15 @@
 package com.pinyougou.order.service.impl;
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
 
+import com.pinyougou.mapper.TbOrderItemMapper;
 import com.pinyougou.mapper.TbOrderMapper;
 import com.pinyougou.order.service.OrderService;
 import com.pinyougou.pojo.TbOrder;
+import com.pinyougou.pojo.TbOrderItem;
+import com.pinyougou.pojogroup.Cart;
+import com.utils.IdWorker;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.github.pagehelper.Page;
@@ -12,6 +18,8 @@ import com.pinyougou.pojo.TbOrderExample;
 import com.pinyougou.pojo.TbOrderExample.Criteria;
 
 import entity.PageResult;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * 服务实现层
@@ -19,10 +27,14 @@ import entity.PageResult;
  *
  */
 @Service
+@Transactional
 public class OrderServiceImpl implements OrderService {
 
 	@Autowired
 	private TbOrderMapper orderMapper;
+
+	@Autowired
+	private TbOrderItemMapper orderItemMapper;
 	
 	/**
 	 * 查询全部
@@ -42,12 +54,53 @@ public class OrderServiceImpl implements OrderService {
 		return new PageResult(page.getTotal(), page.getResult());
 	}
 
+	@Autowired
+	private RedisTemplate redisTemplate;
+
+	@Autowired
+	private IdWorker idWorker;
 	/**
 	 * 增加
 	 */
 	@Override
 	public void add(TbOrder order) {
-		orderMapper.insert(order);		
+		// 1.从缓存redis中提取购物车列表
+		List<Cart>  cartList = (List<Cart>) redisTemplate.boundHashOps("cartList").get(order.getUserId());
+		//2.循环购物车列表添加订单
+		for (Cart cart : cartList) {
+			TbOrder tbOrder = new TbOrder();
+			long orderId = idWorker.nextId(); // 获取id
+			tbOrder.setOrderId(orderId); // 订单id
+
+			tbOrder.setPaymentType(order.getPaymentType()); //支付类型，1、在线支付，2、货到付款
+			tbOrder.setStatus("1"); // 未付款
+			tbOrder.setCreateTime(new Date()); // 订单创建时间
+			tbOrder.setUpdateTime(new Date()); // 订单更新时间
+			tbOrder.setUserId(order.getUserId()); // 用户id
+			tbOrder.setReceiverAreaName(order.getReceiverAreaName()); //收货人地区名称(省，市，县)街道
+			tbOrder.setReceiverMobile(order.getReceiverMobile()); // 收货人手机
+			tbOrder.setReceiver(order.getReceiver()); // 收货人
+			tbOrder.setSourceType(order.getSourceType()); // 订单来源：1:app端，2：pc端，3：M端，4：微信端，5：手机qq端
+			tbOrder.setSellerId(order.getSellerId()); // 商家ID
+
+			double money = 0;
+			// 循环购物车中每条明细记录
+			for (TbOrderItem orderItem : cart.getOrderItemList()) {
+				orderItem.setId(orderId); // 订单id
+				orderItem.setId(idWorker.nextId()); // 主键
+				orderItem.setSellerId(orderItem.getSellerId()); // 商家ID
+				orderItemMapper.insert(orderItem);
+				money += orderItem.getTotalFee().doubleValue();
+			}
+
+			tbOrder.setPayment(new BigDecimal(money)); //合计金额
+
+			orderMapper.insert(tbOrder); //将order添加到数据库
+		}
+
+		//3.清除redis中的购物车
+		redisTemplate.boundHashOps("cartList").delete(order.getUserId());
+
 	}
 
 	
