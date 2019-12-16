77614f2a115d46aef71b4bcce6f5c74072086e44
Howay <heweihzl@163.com>
2019-12-16 20:55:33 +0800

    查询订单状态controller层成功

--- pinyougou-cart-web/src/main/java/com/pinyougou/cart/controller/PayController.java
+++ pinyougou-cart-web/src/main/java/com/pinyougou/cart/controller/PayController.java
@@ -4,9 +4,11 @@ package com.pinyougou.cart.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.pinyougou.pay.service.WeixinPayService;
 import com.utils.IdWorker;
+import entity.Result;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -21,5 +23,22 @@ public class PayController {
         IdWorker idWorker = new IdWorker();
         Map nativeMap = weixinPayService.createNative(String.valueOf(idWorker.nextId()), "1");
         return nativeMap;
+    }
+
+    @RequestMapping("/queryPayStatus")
+    public Result queryPayStatus(String out_trade_no){
+        Result result = null;
+        while (true){
+            Map<String,String> map = weixinPayService.queryPayStatus(out_trade_no);// 调用查询
+            if (map == null){
+                result = new Result("支付发生错误",false);
+                break;
+            }
+            if (map.get("trade_state").equals("SUCCESS")){
+                result = new Result("支付成功",true);
+                break;
+            }
+        }
+        return result;
     }
 }
