f2fee1a941eed963a1f28a1d850fa3fc8f3b5522
Howay <heweihzl@163.com>
2019-12-16 20:43:20 +0800

    查询订单状态封装参数、发送请求、获取结果成功

--- pinyougou-pay-service/src/main/java/com/pinyougou/pay/service/impl/WeixinPayServiceImpl.java
+++ pinyougou-pay-service/src/main/java/com/pinyougou/pay/service/impl/WeixinPayServiceImpl.java
@@ -68,40 +68,33 @@ public class WeixinPayServiceImpl implements WeixinPayService {
         }
     }
 
-    /* @Override
-    public Map createNative(String out_trade_no, String total_fee) {
-        // 1.参数封装
-        Map param = new HashMap();
-        param.put("appid",appid); //公众账号ID
-        param.put("mch_id",partner); // 商户号
-        param.put("nonce_str", WXPayUtil.generateNonceStr()); // 随机字符串
-        param.put("body","品优购"); //商品描述
-        param.put("out_trade_no",out_trade_no); //商户订单号
-        param.put("total_fee",total_fee); //标价金额
-        param.put("spbill_create_ip","127.0.0.1"); // 终端IP
-        param.put("notify_url","http://test.itcast.cn"); // 通知地址
-        param.put("trade_type","NATIVE"); //交易类型
 
+    @Override
+    public Map queryPayStatus(String out_trade_no) {
+        // 1.封装参数
+        Map param = new HashMap();
+        param.put("appid",appid);
+        param.put("mch_id",partner);
+        param.put("out_trade_no",out_trade_no);
+        param.put("nonce_str",WXPayUtil.generateNonceStr());
         try {
-            String paramdXml = WXPayUtil.generateSignedXml(param, partnerkey);
-            System.out.println("请求参数："+paramdXml);
-            //2.发送请求
-            HttpClient httpClient = new HttpClient("https://api.mch.weixin.qq.com/pay/unifiedorder");
-            httpClient.setHttps(true);
-            httpClient.setXmlParam(paramdXml);
-            httpClient.post();
-            //3.获取结果
-            String xmlResult = httpClient.getContent();
-            Map<String, String> mapResult = WXPayUtil.xmlToMap(xmlResult);
-            System.out.println("返回结果："+mapResult);
-            Map map = new HashMap();
-            map.put("code_url",mapResult.get("code_url")); // 生成支付二维码连接
-            map.put("out_trade_no",out_trade_no);
-            map.put("total_fee",total_fee);
+            String xmlSignResult = WXPayUtil.generateSignedXml(param, partnerkey);
+            // 2.发送请求
+            HttpClient client = new HttpClient("https://api.mch.weixin.qq.com/pay/orderquery");
+            client.setHttps(true);
+            client.setXmlParam(xmlSignResult);
+            client.post();
+            //3. 获取结果
+            String contentXml = client.getContent();
+            Map<String, String> map = WXPayUtil.xmlToMap(contentXml);
+            System.out.println("调动查询API返回结果："+map);
             return map;
+
         } catch (Exception e) {
             e.printStackTrace();
-            return new HashMap();
+            return null;
         }
-    }*/
+
+    }
+
 }
