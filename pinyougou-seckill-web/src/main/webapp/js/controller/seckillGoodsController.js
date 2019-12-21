48d8c2fc010f2265a365a5fbb37eca6e713bb227
Howay <heweihzl@163.com>
2019-12-21 23:01:15 +0800

    倒计时读秒完成

--- pinyougou-seckill-web/src/main/webapp/js/controller/seckillGoodsController.js
+++ pinyougou-seckill-web/src/main/webapp/js/controller/seckillGoodsController.js
@@ -17,16 +17,35 @@ app.controller('seckillGoodsController' ,function($scope,$location,seckillGoodsS
         seckillGoodsService.findOne(id).success(
             function(response){
                 $scope.entity=response;
+
+                allsecond =Math.floor( (  new Date($scope.entity.endTime).getTime()- (new Date().getTime())) /1000); //总秒数
+                time= $interval(function(){
+                    if(allsecond>0){
+                        allsecond =allsecond-1;
+                        $scope.timeString=convertTimeString(allsecond);//转换时间字符串
+                    }else{
+                        $interval.cancel(time);
+                        alert("秒杀服务已结束");
+                    }
+                },1000);
+
             }
         );
     }
 
-    $scope.second = 10;
-   time = $interval(function () {
-        $scope.second = $scope.second-1;
-        if ($scope.second<=0){
-            $interval.cancel(time);
+
+    //转换秒为   天小时分钟秒格式  XXX天 10:22:33
+    convertTimeString=function(allsecond){
+        var days= Math.floor( allsecond/(60*60*24));//天数
+        var hours= Math.floor( (allsecond-days*60*60*24)/(60*60) );//小数数
+        var minutes= Math.floor(  (allsecond -days*60*60*24 - hours*60*60)/60    );//分钟数
+        var seconds= allsecond -days*60*60*24 - hours*60*60 -minutes*60; //秒数
+        var timeString="";
+        if(days>0){
+            timeString=days+"天 ";
         }
-    },1000)
+        return timeString+hours+":"+minutes+":"+seconds;
+    }
+
 
 });
