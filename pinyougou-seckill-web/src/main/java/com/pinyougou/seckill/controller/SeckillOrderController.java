8fa87ebf23d7a109b80ff03101cd62e70f632e20
Howay <heweihzl@163.com>
2019-12-21 23:38:05 +0800

    秒杀订单,controller层完成

--- pinyougou-seckill-web/src/main/java/com/pinyougou/seckill/controller/SeckillOrderController.java
+++ pinyougou-seckill-web/src/main/java/com/pinyougou/seckill/controller/SeckillOrderController.java
@@ -1,6 +1,7 @@
 package com.pinyougou.seckill.controller;
 import java.util.List;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -101,7 +102,7 @@ public class SeckillOrderController {
 	
 		/**
 	 * 查询+分页
-	 * @param brand
+	 * @param seckillOrder
 	 * @param page
 	 * @param rows
 	 * @return
@@ -110,5 +111,25 @@ public class SeckillOrderController {
 	public PageResult search(@RequestBody TbSeckillOrder seckillOrder, int page, int rows  ){
 		return seckillOrderService.findPage(seckillOrder, page, rows);		
 	}
-	
+
+	@RequestMapping("/submitOrder")
+	public Result submitOrder(Long seckillId) {
+		// 提出当前用户名
+		String userId = SecurityContextHolder.getContext().getAuthentication().getName();
+		if (userId.equals("anonymousUser")) {
+			return new Result("当前用户未登录", false);
+		}
+
+		try {
+			seckillOrderService.submitOrder(seckillId, userId);
+			return new Result("提交订单成功", true);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new Result("提交订单失败", false);
+		}
+
+	}
+
+
+
 }
